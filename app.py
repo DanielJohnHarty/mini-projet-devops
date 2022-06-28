ddffeece81546d1843ad394b76c11dcd3414705b
Daniel Harty <dan.john.harty@gmail.com>
2022-06-28 14:34:16 +0200

    feat: add capitalise route and tests to flask app

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
     return "<h1>Welcome to your capitalising app!</h1>"
+
+@app.route("/capitalise/<input_str>")
+def capitalise(input_str):
+    return f"""
+      <h1>Capitalise {input_str}</h1>
+      <h2>Capitalise {capital_case(input_str)}</h2>
+    """
+
 if __name__ == "__main__":
     app.run()
--- tests.py
+++ tests.py
@@ -1,9 +1,29 @@
 import pytest
+from flask import *
 from capitalise import *
+from app import app
 
+# Unit Tests
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
 
 def test_raises_exception_on_non_string_arguments():
     with pytest.raises(TypeError):
         capital_case(9)
+
+# Integration tests
+@pytest.fixture
+def client():
+    return app.test_client()
+
+def test_home_returns_200(client):
+    resp = client.get('/home')
+    assert resp.status_code == 200
+
+def test_home(client):
+    resp = client.get('/home')
+    assert resp.data == b"<h1>Welcome to your capitalising app!</h1>"
+
+def test_capitalise_dog(client):
+    resp = client.get('/capitalise/dog')
+    assert b"<h2>Capitalise Dog</h2>" in resp.data
